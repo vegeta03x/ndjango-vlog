eb8445b574bb27ce40a72a5ae79b88a46fbf09a7
Ferindra Nugrahendi <fnugrahendi@gmail.com>
2015-08-08 11:09:48 +0700

    create view and template to display post list

--- vlog/views.py
+++ vlog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 # Create your views here.
+
+def post_list(request):
+	posts = Post.objects.order_by('published_date')
+	return render(request, 'vlog/post_list.html', {'posts' : posts})
