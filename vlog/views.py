9170b5b2e44fe8cdfc3328c7e76f96e7f3057a68
Ferindra Nugrahendi <fnugrahendi@gmail.com>
2015-08-10 20:59:15 +0700

    create add post method

--- vlog/views.py
+++ vlog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from .form import PostForm
 # Create your views here.
 
 def post_list(request):
@@ -11,3 +13,16 @@ def post_list(request):
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'vlog/post_detail.html', {'post' : post})
+
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('vlog.views.post_detail', pk=post.pk)	
+	else:
+		form = PostForm()
+	return render(request, 'vlog/post_edit.html', {'form' : form})
